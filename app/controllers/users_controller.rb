f2674620260adee057df009c350ea325f85cdb5f
Tom Wire <tom@thesilverbadger.co.uk>
2013-06-24 20:57:51 +0100

    allow anonymous signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
 
+  skip_before_filter :require_login
   def new
     @user = User.new
   end
 
+  skip_before_filter :require_login
   def create
     @user = User.new(params[:user])
     if @user.save
