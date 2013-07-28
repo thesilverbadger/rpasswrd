9c1c65352be3870d637195b0c3b26789f40633be
Tom Wire <tom@thesilverbadger.co.uk>
2013-07-28 19:19:43 +0100

    fixed issue where email was case sensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :password, :on => :create
   has_many(:codes)
+
+  before_save do self.email.downcase! end
+
+  def self.find_by_lower_email(email)
+    User.find_by_email(email.downcase)
+  end
 end
