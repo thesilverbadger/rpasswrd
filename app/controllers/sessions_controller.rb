64ac4180636bc04bab6bbc24cc83c1ebe7485eca
Tom Wire <tom@thesilverbadger.co.uk>
2013-08-03 18:15:13 +0100

    added toastr via cdn.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by_lower_email(params[:email].downcase)
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
+      redirect_to root_url
     else
       flash.now.alert = "Invalid email or password"
       render "new"
