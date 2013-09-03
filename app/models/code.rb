a8d2fc53d381cccdda7792609ac82e2a2f5a78d7
Tom Wire <tom@thesilverbadger.co.uk>
2013-09-03 12:09:20 +0100

    added model validation for code and password

--- app/models/code.rb
+++ app/models/code.rb
@@ -1,5 +1,7 @@
 class Code < ActiveRecord::Base
   attr_accessible :code, :is_deleted, :username, :password
   attr_encrypted :password, :key => 'zaqxswcdevfrbgtnhymjukilop1234567890'
+  validates_presence_of(:code)
+  validates_presence_of(:password)
   has_one(:user)
 end
