f6a0e74fa837a7dd3adae60410b6860d1f854fdf
Tom Wire <tom@thesilverbadger.co.uk>
2013-09-04 22:14:24 +0100

    tweaked code creation to not create duplicates

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -10,13 +10,18 @@ class ApiController < ApplicationController
   def add_code
     user = User.find_by_email(params[:email])
 
-    code = Code.new
-    code.user_id = user.id
-    code.password = params[:password]
-    code.username = params[:username]
-    code.code = params[:code]
-    code.is_deleted = false
-    code.save
+    code = Code.find_by_code(params[:code])
+
+    if !code then
+      code = Code.new
+      code.user_id = user.id
+      code.password = params[:password]
+      code.username = params[:username]
+      code.code = code_name
+      code.is_deleted = false
+      code.save
+    end
+
 
     respond_to do |format|
       format.json { render json: user }
