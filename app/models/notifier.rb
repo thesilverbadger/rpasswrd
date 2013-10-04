45ed7e69490d09bec97fe2870072cfad4204cd86
Tom Wire <tom@thesilverbadger.co.uk>
2013-10-04 19:40:31 +0100

    added email template for signup

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -3,6 +3,8 @@ class Notifier < ActionMailer::Base
 
   # send a signup email to the user, pass in the user object that contains the user's email address
   def signup_email(user)
+    @user = user
+    @url  = 'https://secure.tomwire.co.uk/login'
     mail( :to => user.email,
     :subject => 'Thanks for signing up' )
   end
